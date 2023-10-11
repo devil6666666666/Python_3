522a6a4f50e7e440dae131069c51d9ab044b449e
devil6666666666 <74911631+devil6666666666@users.noreply.github.com>
2023-10-11 18:45:13 +0300

    Домашнее задание 8

---
+++ Homework_8.py
@@ -0,0 +1,52 @@
+# 📌 Напишите функцию, которая получает на вход директорию и рекурсивно
+# обходит её и все вложенные директории. Результаты обхода сохраните в
+# файлы json, csv и pickle.
+# ○ Для дочерних объектов указывайте родительскую директорию.
+# ○ Для каждого объекта укажите файл это или директория.
+# ○ Для файлов сохраните его размер в байтах, а для директорий размер
+# файлов в ней с учётом всех вложенных файлов и директорий.
+# 📌 Соберите из созданных на уроке и в рамках домашнего задания функций
+# пакет для работы с файлами разных форматов.
+
+import os
+import json
+import csv
+import pickle
+
+def get_size(path):
+    total = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total += os.path.getsize(fp)
+    return total
+
+def directory_walker(dir_path):
+    results = []
+    for root, dirs, files in os.walk(dir_path):
+        for name in files:
+            full_path = os.path.join(root, name)
+            results.append({"parent_directory": root,
+                            "is_file": True,
+                            "name": name,
+                            "size_in_bytes": os.path.getsize(full_path)})
+
+        for name in dirs:
+            full_path = os.path.join(root, name)
+            results.append({"parent_directory": root,
+                            "is_file": False,
+                            "name": name,
+                            "size_in_bytes": get_size(full_path)})
+
+    with open("output.json", "w") as json_file:
+        json.dump(results, json_file)
+
+    with open("output.csv", "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=results[0].keys())
+        writer.writeheader()
+        writer.writerows(results)
+
+    with open("output.pickle", "wb") as pickle_file:
+        pickle.dump(results, pickle_file)
+
+directory_walker("./my_folder")
